58966eb703f3508dd87eb6f5d0e6fd5f4a0899f7
RakeshSR10 <rakeshsrking@gmail.com>
2021-04-08 09:22:17 +0530

    added Class and Method to TicTocToeGame.java

--- TicTocToeGame.java
+++ TicTocToeGame.java
@@ -1,1 +1,21 @@
 //Welcome to TicTocToeGame Computation Program
+public class TicTocToeGame
+{
+        static char board[][] = new char[3][3];
+        //display function
+        static void Display(char board[][])
+        {
+                for(int i=0; i<3; i++)
+                {
+                        for(int j=0; j<3; j++)
+                        {
+                                System.out.print();
+                        }
+                        System.out.println();
+                }
+        }
+        public static void main(String[] args)
+        {
+                Display(board);
+        }
+}
